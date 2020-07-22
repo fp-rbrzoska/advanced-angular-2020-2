20a41b3730a46dab8eeb0c8ccbe8f48430094cd9
Rafal Brzoska <rbrzoska@future-processing.com>
2020-07-22 13:29:46 +0200

    View children products

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  private isLoggedInSubj = new BehaviorSubject<boolean>(false);
+  get isLoggedIn() {
+    return this.isLoggedInSubj.value;
+  };
+  isLoggedIn$ = this.isLoggedInSubj.asObservable();
   constructor() { }
+
+  logIn() {
+    this.isLoggedInSubj.next(true);
+  }
+
+  logOut() {
+    this.isLoggedInSubj.next(false);
+  }
 }
--- src/app/products/products-list/products-list.component.ts
+++ src/app/products/products-list/products-list.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, QueryList } from '@angular/core';
 import { ProductsService } from '../products.service';
 import { Product } from '../../models/product';
 import { Observable } from 'rxjs';
+import { ProductsItemComponent } from '../products-item/products-item.component';
 
 @Component({
   selector: 'fp-products-list',
@@ -11,12 +12,18 @@ import { Observable } from 'rxjs';
 export class ProductsListComponent implements OnInit {
 
   products$: Observable<Product[]>;
+
+  @ViewChildren(ProductsItemComponent) productItems: QueryList<ProductsItemComponent>;
   constructor(private productService: ProductsService) {
     this.products$ = this.productService.productsData$;
     this.productService.fetchData();
   }
 
   ngOnInit(): void {
+  }
+
+  toggleDetails() {
+    this.productItems.forEach(p => p.showDetails = !p.showDetails);
   }
 
 }
